a547fddd148e408593e39aadb47916ede37659d1
Michael Bradley, Jr. <michaelsbradleyjr@gmail.com>
2024-01-17 08:58:24 -0600

    [wip] expand examples/achan1

--- examples/achan1.nim
+++ examples/achan1.nim
@@ -2,27 +2,66 @@
 
 import pkg/[achan, chronos]
 
+type
+  XKinds = enum
+    xkA, xkB, xkC
+
+  X = ref object
+    case kind: XKinds
+    of xkA:
+      a: string
+    of xkB:
+      b: int
+    of xkC:
+      c: Y
+
+  Y = object
+    s: string
+
+proc recvX(c: AsyncChannel[X]) {.async.} =
+  let rdata = await c.recv
+  case rdata.kind
+  of xkA:
+    echo "string: " & rdata.a
+  of xkB:
+    echo "int: " & $rdata.b
+  else:
+    echo "rdata: " & $rdata[]
+
+proc sendX[T](c: AsyncChannel[X], val: T) {.async.} =
+  when T is string:
+    await c.send(X(kind: xkA, a: val))
+  elif T is int:
+    await c.send(X(kind: xkB, b: val))
+  elif T is Y:
+    await c.send(X(kind: xkC, c: val))
+  else:
+    {.error: "unsupported type '" & $typeof(T) & "' for sendX()".}
+
 proc amain() {.async.} =
-  let chan = achan[int]()
+  let chan = achan[X]()
 
-  let sdata = 123
-  await chan.send(sdata)
+  discard chan.recvX
+  await chan.sendX("abc")
 
-  let rdata = await chan.recv
-  echo rdata
+  await chan.sendX(123)
+  await chan.recvX
+
+  await chan.sendX(Y(s: "def"))
+  await chan.recvX
 
   chan.close
 
 proc main() =
   try:
-    echo ""
     waitFor amain()
-    echo ""
   except CatchableError as e:
     raise (ref Defect)(msg: e.msg)
 
 when isMainModule:
+  echo ""
   main()
+  echo ""
 
 
 # NOTES: while working on achan & examples/tests
